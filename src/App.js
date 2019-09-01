f9b2784a02b94385cacc8420a39d5e7631994cb8
EC2 Default User <ec2-user@ip-172-30-1-116.us-east-2.compute.internal>
2019-09-01 16:05:29 +0000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There!!
+          Feature branch intruduced !!
         </p>
         <a
           className="App-link"
